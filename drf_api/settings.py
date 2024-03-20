234e168bf472f291f64cff85610d2d77a9fba9f6
MidnattLantern <isakvent@icloud.com>
2024-03-20 14:08:38 +0100

    added proper url to allwoed host and switched debug to not True

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -60,13 +60,13 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
+
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     'localhost',
-    'https://ci-drfapi-tutorial-3-a9f788ee8efe.herokuapp.com'
+    'ci-drfapi-tutorial-3-a9f788ee8efe.herokuapp.com',
 ]
 
 
